7675373b17c69b00ae3753dbfde488ac26ff1c1c
gabiikerniske <112017069+gabiikerniske@users.noreply.github.com>
2024-04-09 11:28:27 -0300

    0904

--- main.js
+++ main.js
@@ -13,22 +13,34 @@ for (let i=0; i< botoes.length; i++){
 }
 const tempoObjetivo1 = new Date("2024-02-04T00:00:00");
 const tempoObjetivo2 = new Date("2024-12-07T00:00:00");
-const tempoObjetivo3 = new Date("2024-12-06T00:00:00");
-const tempoObjetivo4 = new Date("2024-12-09T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-09T00:00:00");
+const tempoObjetivo4 = new Date("2024-12-011T00:00:00");
 const tempo = [ tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
-for (let i = 0; i < contadores.length; i++) {}
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+function atualizaCronometro(){
+for (let i = 0; i < contadores.length; i++) {
+contadores[i].textContent = calculaTempo(tempos[i]);
+}
+}
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro,1000);
+}
+comecaCronometro();
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
     let segundos = Math. floor(tempofinal / 1000);
     let minutos = Math.floor(segundos/ 60);
-    let horas = Math.floor(minutos/60);
+    let horas = Math.floor(minutos/ 60);
     let dias = Math.floor(horas/ 24);
 
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
    
+    if (tempoFinal > 0){
     return dias + "dias" + horas + "horas" + minutos + "minutos" + segundos + "segundos";
+    }else {
+        return "Prazo Finalizado";
+    }
 }
