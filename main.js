dca60ba54cf75e96f93645afa30ae43c2d480056
gabiikerniske <112017069+gabiikerniske@users.noreply.github.com>
2024-04-09 11:40:10 -0300

    0904

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ const tempoObjetivo1 = new Date("2024-02-04T00:00:00");
 const tempoObjetivo2 = new Date("2024-12-07T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-09T00:00:00");
 const tempoObjetivo4 = new Date("2024-12-011T00:00:00");
-const tempo = [ tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+const tempos = [ tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 function atualizaCronometro(){
 for (let i = 0; i < contadores.length; i++) {
 contadores[i].textContent = calculaTempo(tempos[i]);
